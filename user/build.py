5f8c36ee6916ccc88493db5ce6d210b72011dec7
kingbultsea <894275157@qq.com>
2023-07-06 11:41:08 +0800

    😯

--- user/build.py
+++ user/build.py
@@ -8,6 +8,7 @@ app_id = 0
 apps = os.listdir('src/bin')
 apps.sort()
 for app in apps:
+    # 截取从字符串开头到第一个出现的点号（.）之前的部分，cargo build --bin xxxx  不需要.后缀
     app = app[:app.find('.')]
     lines = []
     lines_before = []
@@ -18,8 +19,10 @@ for app in apps:
             lines.append(line)
     with open(linker, 'w+') as f:
         f.writelines(lines)
+    # 该命令会删除被修改后的linker.ld
     os.system('cargo build --bin %s --release' % app)
     print('[build.py] application %s start with address %s' %(app, hex(base_address+step*app_id)))
+    # 恢复linker.ld
     with open(linker, 'w+') as f:
         f.writelines(lines_before)
     app_id = app_id + 1
--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -4,14 +4,20 @@ const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
 
+// s0 -> s11函数是保存寄存器
+// s0是sp寄存器，用于debugger
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
     unsafe {
+        // x10~x17 : 对应 a0~a7
         asm!(
+            // Environment Call 用户态程序和操作系统之间进行通讯
             "ecall",
             inlateout("x10") args[0] => ret,
+            // 把args[1]传递进a1寄存器中
             in("x11") args[1],
             in("x12") args[2],
+            // 系统调用id
             in("x17") id
         );
     }
@@ -19,6 +25,9 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
 }
 
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    // a0为字符串地址 这里的fd为1
+    // a1 为字符串长度 
+    // a7 为 64 
     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
 
